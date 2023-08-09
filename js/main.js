02c869eed51e2a3b57f5eeede48aba4eee210b00
csc20S016 <109369573+csc20S016@users.noreply.github.com>
2023-08-09 20:47:15 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ $(window).on('load', function () {
 
 });
 
+
 function ajaxLoad(){
 
   header_options();
@@ -468,7 +469,26 @@ function testimonialSlider(){
   
   }//End ContactForm
 
+  const toggles = document.querySelectorAll('.faq-toggle');
 
+  toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+      toggle.parentNode.classList.toggle('active');
+    });
+  });
+  
+  // SOCIAL PANEL JS
+  const floating_btn = document.querySelector('.floating-btn');
+  const close_btn = document.querySelector('.close-btn');
+  const social_panel_container = document.querySelector('.social-panel-container');
+  
+  floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+  });
+  
+  close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+  });
 
 
 
